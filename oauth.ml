1ccf20332765343cb10e83c9de72d2d5f3d26ba4
Guillaume Louvigny <tuxkowo@gmail.com>
2012-08-29 08:52:32 +0200

    Removed OCaml objects - Supports Facebook, Google, Github, Foursquare

--- oauth.ml
+++ oauth.ml
@@ -67,10 +67,11 @@ type oAuthUserStatus =
    api_base_url string OAuth API base URL, for more simple API calls
    oauth_version oAuthVersion OAuth version used by API
  *)
-type oAuthAPI = { 
+type oAuthEndpoint = { 
   api_login_url : string;
   api_token_url : string;
   api_base_url : string;
+  auth_function : (string -> oAuthUserStatus);
   oauth_version : oAuthVersion;
 }
 
@@ -79,272 +80,221 @@ type oAuthAPI = {
  *)
 type oAuthPermission = (string)
 
-class oAuthUser = fun ?(status = LoggedOut) ?(permissions = [""]) () ->
-object (self)
-    val mutable status = status
-    val mutable permissions = permissions
+type oAuthClient = {
+  id : string;
+  secret : string;
+  identifier : string;
+  state : string;
+  endpoint : oAuthEndpoint;
+}
 
-    (**
-       Get user's status
+type oAuthUser = {
+  status : oAuthUserStatus;
+  permissions : oAuthPermission list;
+  api_client : oAuthClient;
+}
 
-       @return oAuthUserStatus
-     *)
-    method getStatus () = status
+(**
+   Generate a md5 from a random int
 
-    (**
-       Get user's permissions
+   @return string
+*)
+let random_token () = Digest.to_hex (
+  Digest.string (
+    Int32.to_string (
+      Random.int32 (
+        Int32.max_int))))
 
-       @return (oAuthPermission) list
-     *)
-    method getPermissions () = permissions
+(**
+   Find an element in a key * value list
 
-    (**
-       Set user's OAuth code
+   @param needle element to be found
+   @param l (string * string) list l
+   @raise Failure Fails when key not found
+   @return string
+*)
+let rec find_param needle l = match List.hd l with
+  | (name, value) when ((String.compare name needle) == 0) -> value
+  | _ -> find_param needle (List.tl l)
 
-       @param code OAuth Code
-       @return err?
-     *)
-    method setCode code = status <- Code (code)
+(**
+   Set user's status
 
-    (**
-       Set user's access token
+   @param oauth_user User to change status
+   @param new_status User new status
+*)
+let set_user_status oauth_user new_status = {
+  status = new_status;
+  permissions = oauth_user.permissions;
+  api_client = oauth_user.api_client;
+}
 
-       @param token OAuth Access Token
-       @param int Access Token expiration timestamp
-       @return err?
-     *)
-    method setAccessToken token expires = status <-  Token (token, expires)
+(**
+   Get a login URL for OAuth API.
 
-    (**
-       Mark user as logged out
+   @param oAuthPermission list scope
+   @param string redirect_url
+   @return string
+*)
+let login_url api_user redirect_url = api_user.api_client.endpoint.api_login_url ^ "?" ^
+  Netencoding.Url.mk_url_encoded_parameters [
+    ("client_id",  api_user.api_client.id) ;
+    ("redirect_uri",  redirect_url) ;
+    ("scope", String.concat " " api_user.permissions) ;
+    ("response_type", "code") ;
+    ("state",  api_user.api_client.state) ;
+  ]
 
-       @return err?
-     *)
-    method setLoggedOut () = status <- LoggedOut
+(**
+   Get access token for current user, if user is not logged returns
+   application Access Token
 
-    (**
-       Set user's status
+   @return string
+*)
+let api_access_token api_client = api_client.id ^ "|" ^ api_client.secret
 
-       @param new_status User new status
-       @return err?
-     *)
-    method setStatus new_status = status <- new_status
-end
+(**
+   Params to exchange code to a token.
 
-class oAuthClient =
-  fun endpoint
-    (id : string)
-    (secret : string)
-    ?(identifier = "")
-    ?(state = "")
-    () ->
+   @param oAuthPermission list scope
+   @param string redirect_url
+*)
+let access_token_params api_client redirect_url code = 
+  [
+    ("client_id", api_client.id) ;
+    ("client_secret", api_client.secret) ;
+    ("redirect_uri",  redirect_url) ;
+    ("grant_type", "authorization_code") ;
+    ("code", code) ;
+  ]
 
-  object (self)
-      val id = id
-      val secret = secret
-      val endpoint = endpoint
-      val identifier = identifier
-      val user = new oAuthUser ()
+(**
+   Parse an access token stored in a JSON object.
+   User object is not modified as external methods may be used.
 
-      val mutable state = state
-
-      (**
-         Generate a md5 from a random int
-
-         @return string
-       *)
-      method randomToken () = Digest.to_hex (
-        Digest.string (
-          Int32.to_string (
-            Random.int32 (
-              Int32.max_int))))
-
-      (**
-         Find an element in a key * value list
-
-         @param needle element to be found
-         @param l (string * string) list l
-         @raise Failure Fails when key not found
-         @return string
-       *)
-      method findParam needle l = match List.hd l with
-      | (name, value) when ((String.compare name needle) == 0) -> value
-      | _ -> self#findParam needle (List.tl l)
-
-      (**
-         Get user
-
-         @return oAuthUser
-       *)
-      method getUser () = user
-
-      (**
-         Get identifier
-
-         @return string
-       *)
-      method getIdentifier () = identifier
-
-      (**
-         Return current state (csrf unique token)
-         Generate one if not found
-
-         @return string
-       *)
-      method getState () =
-          if ((String.length state) == 0) then 
-            state <- self#randomToken ();
-          state
-
-      (**
-         Get a login URL for OAuth API.
-
-         @param oAuthPermission list scope
-         @param string redirect_url
-         @return string
-       *)
-      method getLoginUrl scope redirect_url = endpoint.api_login_url ^ "?" ^
-          Netencoding.Url.mk_url_encoded_parameters [
-              ("client_id",  id) ;
-              ("redirect_uri",  redirect_url) ;
-              ("scope", (String.concat " " scope)) ;
-              ("response_type", "code") ;
-              ("state",  (self#getState ())) ;
-          ]
-
-      (**
-         Get access token for current user, if user is not logged returns
-         application Access Token
-
-         @return string
-       *)
-      method getAccessToken () = match user#getStatus () with
-          | Token (token, _) -> token
-          | _ -> id ^ "|" ^ secret
-
-      (**
-         Get access token for current user, if user is not logged returns
-         application Access Token
-
-         @return string
-       *)
-      method getCode () = match user#getStatus () with
-          | Code (code) -> code
-          | _ -> failwith "No code defined"
-
-      (**
-         Get an URL to exchange code to a token.
-
-         @param oAuthPermission list scope
-         @param string redirect_url
-       *)
-      method getAccessTokenUrl redirect_url code = 
-        endpoint.api_token_url ^ "?" ^
-            Netencoding.Url.mk_url_encoded_parameters [
-                ("client_id", id) ;
-                ("client_secret", secret) ;
-                ("redirect_uri",  redirect_url) ;
-                ("grant_type", "authorization_code") ;
-                ("code", code) ;
-            ]
-
-      (**
-        Parse an access token stored in a JSON object.
-        User object is not modified as external methods may be used.
-
-        @return (string, int) pair
-       *)
-      method jsonEncodedTokenAccess (response : http_call) = 
-        let json = Yojson.Basic.from_string (response#get_resp_body ()) in
-        let access_token = 
-          List.hd ([json]
-          |> filter_member "access_token"
-          |> filter_string
-          ) in
-        let expires = 
-          List.hd ([json]
-          |> filter_member "expires_in"
-          |> filter_number
-          ) in
-        (access_token, int_of_float expires)
+   @return Token (string, int)
+*)
+let decode_json_token_access data = 
+  let json = Yojson.Basic.from_string data in
+  let access_token = 
+    List.hd ([json]
+		|> filter_member "access_token"
+		|> filter_string
+    ) in
+  let expires = 
+    try
+      List.hd ([json]
+		  |> filter_member "expires_in"
+		  |> filter_number
+    ) 
+    with
+	_ -> 0.0 in
+  Token (access_token, int_of_float expires)
 
 
-      (**
-        Parse an access token stored in an URL encoded-style answer.
-        User object is not modified as external methods may be used.
-        This is mainly intended to be used with Facebook, which doesn't follow
-        recent OAuth 2 RFC.
+(**
+   Parse an access token stored in an URL encoded-style answer.
+   User object is not modified as external methods may be used.
+   This is mainly intended to be used with Facebook, which doesn't follow
+   recent OAuth 2 RFC.
 
-	 Use it this way:
-	 ~response_parse: (api_client#urlEncodedTokenAccess)
+   @return Token (string, int)
+*)
+let decode_url_encoded_access_token data = 
+  let resp_list = Netencoding.Url.dest_url_encoded_parameters
+    data in
+  let token = find_param "access_token" resp_list in
+  let expires =
+    try
+      int_of_string (find_param "expires" resp_list)
+    with
+      | _ -> 0 in
+  Token (token, expires)
 
-        @return (string, int) pair
-       *)
-      method urlEncodedTokenAccess (response : http_call) = 
-        let resp_list = Netencoding.Url.dest_url_encoded_parameters
-          (response#get_resp_body ()) in
-        let token = self#findParam "access_token" resp_list in
-        let expires = int_of_string (self#findParam "expires" resp_list) in
-          (token, expires)
+(**
+   Exchange a code for an access token
+   Uses jsonEncodedTokenAccess as default response parser (OAuth 2 d. 26)
 
-      (**
-         Exchange a code for an access token
-         Uses jsonEncodedTokenAccess as default response parser (OAuth 2 d. 26)
+   @return unit
+*)
+let exchange_code_for_access_token
+    user
+    redirect_url
+    () =
+  let response_parse = user.api_client.endpoint.auth_function in
+  match user.status with
+    | Code (code) ->
+      let params = access_token_params user.api_client redirect_url code in
+      let response = http_post_message user.api_client.endpoint.api_token_url params in
+      (match response#response_status_code with
+	| x when (x < 400) -> set_user_status user (response_parse (response#get_resp_body ()))
+	| _ -> raise (OAuthException (400, ("Failure on HTTP request:" ^ (response#get_resp_body ())))))
+    | _ -> raise (OAuthException (400, "User has no code"))
+	  
+(**
+   Appends params to url (for GET, PUT, DELETE)
 
-         @return unit
-       *)
-      method exchangeCodeForAccessToken
-      	redirect_url
-        ?(response_parse = self#jsonEncodedTokenAccess)
-      	?(code = self#getCode ()) () =
-          let url = self#getAccessTokenUrl redirect_url code in
-          let response = http_post_message url [] in
-          match response#response_status_code with
-          | 200 -> (
-              match response_parse response with
-              | (token, expires) -> user#setAccessToken token, expires
-            )
-          | _ -> raise (OAuthException (400, "Failure on HTTP request"))
+   @return string
+*)
+let params_to_url url params =
+  url ^ "?" ^ Netencoding.Url.mk_url_encoded_parameters params
 
-      (**
-        Appends params to url (for GET, PUT, DELETE)
+(**
+   Do an API call
+   @todo
+*)
+let api
+    user
+    action
+    ?(http_method = GET)
+    ?(http_params = [])
+    ?(http_content = "")
+    () =
+  let access_token = match user.status with
+    | Token (token, expires) -> token
+    | _ -> api_access_token user.api_client in
+  let http_params = (* appends access token to parameters *)
+    http_params@
+      [
+	("access_token", access_token);
+	("oauth_token", access_token); (* silly hack to support foursquare*)
+      ] in
+  let http_url =  (* check if a full URL is given *)
+    if Str.string_match (Str.regexp "^https://") action 0 then
+      action
+    else
+      user.api_client.endpoint.api_base_url ^ action
+  in
+  let http_response = match http_method with
+    | GET -> 
+      print_endline (params_to_url http_url http_params);
+      http_get_message (params_to_url http_url http_params)
+    | POST -> http_post_message http_url http_params
+    | PUT -> http_put_message
+      (params_to_url http_url http_params) http_content
+    | DELETE -> http_delete_message
+      (params_to_url http_url http_params)
+  in
+  match http_response#response_status_code with
+    | 200 -> http_response#get_resp_body ()
+    | _ -> raise (OAuthException (400, "Failure on HTTP request"))
 
-        @return string
-       *)
-      method paramsToUrl url params =
-        url ^ "?" ^ Netencoding.Url.mk_url_encoded_parameters params
 
-      (**
-        Do an API call
-        @todo
-       *)
-      method api
-        action
-        ?(http_method = GET)
-        ?(http_params = [])
-        ?(http_content = "")
-        () =
-        let http_params = (* appends access token to parameters *)
-          http_params@[("access_token", self#getAccessToken ())] in
-        let http_url =  (* check if a full URL is given *)
-          if Str.string_match (Str.regexp "^https://") action 0 then
-            action
-          else
-            endpoint.api_base_url ^ action
-          in
-        let http_response = match http_method with
-          | GET -> http_get_message (self#paramsToUrl http_url http_params)
-          | POST -> http_post_message http_url http_params
-          | PUT -> http_put_message
-            (self#paramsToUrl http_url http_params) http_content
-          | DELETE -> http_delete_message
-            (self#paramsToUrl http_url http_params)
-        in
-        match http_response#response_status_code with
-          | 200 -> http_response#get_resp_body ()
-          | _ -> raise (OAuthException (400, "Failure on HTTP request"))
+let make_api_client id secret endpoint ?(identifier = "") ?(state = (random_token ())) () =
+  {
+    id = id;
+    secret = secret;
+    identifier = identifier;
+    state = state;
+    endpoint = endpoint;
+  }
 
-  end
+let make_api_user api_client ?(status = LoggedOut) ?(permissions = []) () = 
+  {
+    status = status;
+    permissions = permissions;
+    api_client = api_client;
+  }
 
 (*
    OAUTH ENDPOINT EXAMPLES
@@ -358,6 +308,7 @@ let facebook_oauth_endpoint = {
   api_login_url = "https://www.facebook.com/dialog/oauth";
   api_token_url = "https://graph.facebook.com/oauth/access_token";
   api_base_url = "https://graph.facebook.com/";
+  auth_function = decode_url_encoded_access_token;
   oauth_version = OAUTH_2_D10;
 }
 
@@ -369,6 +320,7 @@ let google_oauth_endpoint = {
   api_login_url = "https://accounts.google.com/o/oauth2/auth";
   api_token_url = "https://accounts.google.com/o/oauth2/token";
   api_base_url = "https://www.googleapis.com/oauth2/v1/";
+  auth_function = decode_json_token_access;
   oauth_version = OAUTH_2_D10;
 }
 
@@ -380,17 +332,7 @@ let github_oauth_endpoint = {
   api_login_url = "https://github.com/login/oauth/authorize";
   api_token_url = "https://github.com/login/oauth/access_token";
   api_base_url = "https://api.github.com/";
-  oauth_version = OAUTH_2_D10;
-}
-
-(*
-  Register your Meetup app at
-  http://www.meetup.com/meetup_api/oauth_consumers/create/
-*)
-let meetup_oauth_endpoint = {
-  api_login_url = "https://secure.meetup.com/oauth2/authorize";
-  api_token_url = "https://secure.meetup.com/oauth2/access";
-  api_base_url = "http://api.meetup.com/";
+  auth_function = decode_url_encoded_access_token;
   oauth_version = OAUTH_2_D10;
 }
 
@@ -401,7 +343,24 @@ let meetup_oauth_endpoint = {
 let foursquare_oauth_endpoint = {
   api_login_url = "https://foursquare.com/oauth2/authenticate";
   api_token_url = "https://foursquare.com/oauth2/access_token";
-  api_base_url = "https://api.foursquare.com/v2";
+  api_base_url = "https://api.foursquare.com/v2/";
+  auth_function = decode_json_token_access;
+  oauth_version = OAUTH_2_D10;
+}
+
+(*
+  Endpoints below are not tested yet
+*)
+
+(*
+  Register your Meetup app at
+  http://www.meetup.com/meetup_api/oauth_consumers/create/
+*)
+let meetup_oauth_endpoint = {
+  api_login_url = "https://secure.meetup.com/oauth2/authorize";
+  api_token_url = "https://secure.meetup.com/oauth2/access";
+  api_base_url = "http://api.meetup.com/";
+  auth_function = decode_json_token_access;
   oauth_version = OAUTH_2_D10;
 }
 
@@ -413,6 +372,7 @@ let microsoft_oauth_endpoint = {
   api_login_url = "https://login.live.com/oauth20_authorize.srf";
   api_token_url = "https://login.live.com/oauth20_token.srf";
   api_base_url = "http://apis.live.net/v5.0/";
+  auth_function = decode_json_token_access;
   oauth_version = OAUTH_2_D10;
 }
 
