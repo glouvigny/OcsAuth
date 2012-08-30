be46f3a654d24bbca1b2f07604fa6b0c2cbbeb58
Guillaume Louvigny <tuxkowo@gmail.com>
2012-08-30 09:32:41 +0200

    Added LICENSE + Instagram

--- oauth.ml
+++ oauth.ml
@@ -1,5 +1,35 @@
 (*
-   Require lib > Netencoding.Url
+
+Copyright (c) 2012, Guillaume Louvigny
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Guillaume Louvigny, nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL GUILLAUME LOUVIGNY BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+   Required libs > Ocamlnet : Netencoding.Url
+                 > Ocamlnet : HttpClient
+                 > equeue-ssl
+                 > YoJson
  *)
 
 (**
@@ -376,3 +406,14 @@ let microsoft_oauth_endpoint = {
   oauth_version = OAUTH_2_D10;
 }
 
+(*
+  Register your Instagram app at
+  
+*)
+let instagram_oauth_endpoint = {
+  api_login_url = "https://api.instagram.com/oauth/authorize/";
+  api_token_url = "https://api.instagram.com/oauth/access_token";
+  api_base_url = "https://api.instagram.com/";
+  auth_function = decode_json_token_access;
+  oauth_version = OAUTH_2_D10;
+}
