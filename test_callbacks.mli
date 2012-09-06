8cab4b92b6e304d592e3ac57551dc75a31686f76
Tuxkowo <glouvigny@users.noreply.github.com>
2012-09-06 23:34:28 +0200

    Added basic list of services login links

---
+++ test_callbacks.mli
@@ -0,0 +1,9 @@
+val google_test : Oauth.oAuthUser -> string 
+val facebook_test : Oauth.oAuthUser -> string
+val github_test : Oauth.oAuthUser -> string
+val ms_test : Oauth.oAuthUser -> string
+val meetup_test : Oauth.oAuthUser -> string
+val instagram_test : Oauth.oAuthUser -> string
+val deezer_test : Oauth.oAuthUser -> string
+val foursquare_test : Oauth.oAuthUser -> string
+
